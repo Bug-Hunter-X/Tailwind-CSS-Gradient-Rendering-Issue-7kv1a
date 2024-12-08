0ca83e3dfd78488e33ac1ae8de9a9af4508fdaa1
BugHunter-X <askzenai@gmail.com>
2024-12-08 12:00:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="bg-gradient-to-r from-blue-500 to-purple-500 p-4 rounded-lg">
+  <!-- Content here -->
+</div>
+```
+This code uses Tailwind's gradient feature.  However, if you're using an older version of Tailwind or have a conflicting configuration, the gradient might not render correctly.  This could manifest as a solid color instead of a gradient or no styling at all.
